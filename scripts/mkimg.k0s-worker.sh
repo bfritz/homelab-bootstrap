d705530691d59ccc6120d88bcf0246ffcde68952
Brad Fritz <github@bfritz.com>
2021-10-26 09:42:22 -0400

    k0s-worker: include atop package (uninstalled) in image

--- scripts/mkimg.k0s-worker.sh
+++ scripts/mkimg.k0s-worker.sh
@@ -4,6 +4,6 @@ profile_k0s_worker() {
     title="Alpine for k0s worker node"
     desc="Standard Alpine image ready to be provisioned as k0s worker node, likely with k0sctl.  Runs from RAM."
     arch="x86_64"
-    apks="$apks chrony openssh-server findutils coreutils curl iptables nfs-utils"
+    apks="$apks chrony openssh-server findutils coreutils curl iptables nfs-utils atop"
     apkovl="genapkovl-k0s-worker.sh"
 }
